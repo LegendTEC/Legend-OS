fae12dabccb81f9b0f5c5308007ad88f0a89d16e
lplegendyt <167509251+lplegendyt@users.noreply.github.com>
2024-05-02 06:12:02 +0000

    Pending changes exported from your codespace

---
+++ main.cs
@@ -0,0 +1,15 @@
+﻿using System.IO;
+
+Start
+
+
+class Start
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Wie lautet dein name :");
+        string username = Console.ReadLine() ?? "";
+        Console.Write("Willkomen" + username);
+    }
+
+}
